61c4b655e7e00466a33e7744a3d96f9dd303beed
fi01 <furu.is01@gmail.com>
2013-07-24 09:54:27 +0900

    Update SC-01E module

--- sc01e/Android.mk
+++ sc01e/Android.mk
@@ -4,11 +4,11 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
   ../kallsyms.c \
-  sec_unlock_sc01e.c \
+  unlock_sec_sc01e.c \
   ../ptmx.c \
   ../backdoor_mmap.c
 
-LOCAL_MODULE := sec_unlock_sc01e
+LOCAL_MODULE := unlock_sec_sc01e
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES += .
 LOCAL_FORCE_STATIC_EXECUTABLE := true
