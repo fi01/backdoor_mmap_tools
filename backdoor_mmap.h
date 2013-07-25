711d32b6e67183ada7d98bf44914aa7ffe8c3c5a
fi01 <furu.is01@gmail.com>
2013-07-25 15:29:52 +0900

    Move mapped base address to 0x20000000

--- backdoor_mmap.h
+++ backdoor_mmap.h
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 
 #define BACKDOOR_MMAP_SIZE    0x0a000000
-#define BACKDOOR_MMAP_ADDRESS 0x10000000
+#define BACKDOOR_MMAP_ADDRESS 0x20000000
 
 extern void *backdoor_convert_to_kernel_address(void *address);
 extern void *backdoor_convert_to_mmaped_address(void *address);
