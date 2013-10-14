2fab97954e712a05c4d671c56a3d7ef58258aa63
fi01 <furu.is01@gmail.com>
2013-10-15 01:54:39 +0900

    Add P-02E 10.0767

--- install_backdoor.c
+++ install_backdoor.c
@@ -38,6 +38,7 @@ static supported_device supported_devices[] = {
   { DEVICE_LT26W_6_2_B_0_200,       0x40208000, 0xc0143f98 },
   { DEVICE_LT29I_9_1_B_0_411,       0x80208000, 0xc01177e0 },
   { DEVICE_N03E_A7202001,           0x80208000, 0xc0112694 },
+  { DEVICE_P02E_10_0767,            0x80208000, 0xc011c6e8 },
   { DEVICE_SBM203SH_S0024,          0x80208000, 0xc00f0e68 },
   { DEVICE_SH02E_02_00_03,          0x80208000, 0xc00f0e84 },
   { DEVICE_SH04E_01_00_02,          0x80208000, 0xc00f10d4 },
