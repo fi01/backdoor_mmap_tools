c41b29f56c98146f88a5ecbeb6b08f08e936afd2
fi01 <furu.is01@gmail.com>
2013-12-19 08:24:55 +0900

    Auto detect params to unlock mmc_protect

--- mmc_protect/unlock_mmc_protect.c
+++ mmc_protect/unlock_mmc_protect.c
@@ -13,6 +13,7 @@
 
 #include "device_database/device_database.h"
 #include "backdoor_mmap.h"
+#include "libkallsyms/kallsyms_in_memory.h"
 
 //#define DISABLE_UNLOCK_MMC_SYSTEM_WRITE
 
@@ -31,6 +32,14 @@
 #define mmc_protect_part_sh09d_02_00_03 0xc075262c
 #define mmc_protect_part_shl21_01_00_09 0xc09b6e58
 #define mmc_protect_part_shl21_01_01_02 0xc074feac
+
+#define ARRAY_SIZE(n)	(sizeof (n) / sizeof ((n)[0]))
+
+typedef enum {
+  MMC_PROTECT_PART_TYPE_UNKNOWN = 0,
+  MMC_PROTECT_PART_TYPE1 = 1,
+  MMC_PROTECT_PART_TYPE2,
+} mmc_protect_part_type_t;
 
 #define MMC_SYSTEM_PARTITION_TYPE1      15
 #define MMC_SYSTEM_PARTITION_TYPE2      12
@@ -81,17 +90,35 @@ static const struct mmc_protect_inf check_mmc_protect_part_type2[] = {
 static int n_mmc_protect_part_type2 = sizeof (check_mmc_protect_part_type2) / sizeof (check_mmc_protect_part_type2[0]);
 
 static unsigned long int mmc_protect_part;
-static const struct mmc_protect_inf *check_mmc_protect_part;
-static int n_mmc_protect_part;
-static int mmc_system_partition;
+static mmc_protect_part_type_t mmc_protect_part_type;
 
 bool
 unlock_mmc_protect_part(void)
 {
+  const struct mmc_protect_inf *check_mmc_protect_part;
+  int n_mmc_protect_part;
+  int mmc_system_partition;
   struct mmc_protect_inf *p;
   int count_readable = 0;
   int count_writable = 0;
   int i;
+
+  switch (mmc_protect_part_type) {
+  case MMC_PROTECT_PART_TYPE1:
+    check_mmc_protect_part = check_mmc_protect_part_type1;
+    n_mmc_protect_part = n_mmc_protect_part_type1;
+    mmc_system_partition = MMC_SYSTEM_PARTITION_TYPE1;
+    break;
+
+  case MMC_PROTECT_PART_TYPE2:
+    check_mmc_protect_part = check_mmc_protect_part_type2;
+    n_mmc_protect_part = n_mmc_protect_part_type2;
+    mmc_system_partition = MMC_SYSTEM_PARTITION_TYPE2;
+    break;
+
+  default:
+    return false;
+  }
 
   p = backdoor_convert_to_mmaped_address((void *)mmc_protect_part);
 
@@ -130,6 +157,111 @@ unlock_mmc_protect_part(void)
   return true;
 }
 
+static bool
+detect_mmc_protect(void)
+{
+  typedef struct {
+    mmc_protect_part_type_t type;
+    const struct mmc_protect_inf *inf;
+    int num;
+  } check_t;
+
+  check_t check[] = {
+    { MMC_PROTECT_PART_TYPE1, check_mmc_protect_part_type1, n_mmc_protect_part_type1 },
+    { MMC_PROTECT_PART_TYPE2, check_mmc_protect_part_type2, n_mmc_protect_part_type2 },
+  };
+
+  kallsyms *info;
+  unsigned long int addr;
+  const struct mmc_protect_inf *p;
+  bool ret = false;
+  int i;
+
+  info = kallsyms_in_memory_init((void *)BACKDOOR_MMAP_ADDRESS, BACKDOOR_MMAP_SIZE);
+  if (info == NULL) {
+    printf("kallsyms_in_memory_init(): failed\n");
+    return false;
+  }
+
+  addr = kallsyms_in_memory_lookup_name(info, "mmc_protect_part");
+  if (!addr) {
+    goto error_exit;
+  }
+
+  printf("Found: mmc_protect_part = 0x%08x\n", addr);
+
+  p = backdoor_convert_to_mmaped_address((void *)addr);
+
+  if (p[0].partition == 0) {
+    p++;
+  }
+
+  for (i = 0; i < ARRAY_SIZE(check); i++) {
+    int n;
+
+    for (n = 0; n < check[i].num; n++) {
+      if (p[n].partition != check[i].inf[n].partition) {
+        break;
+      }
+
+      if (p[n].protect & ~(MMC_PROTECT_READ | MMC_PROTECT_WRITE)) {
+        break;
+      }
+    }
+
+    if (n == check[i].num) {
+      printf("Detect partition type: %d\n", check[i].type);
+
+      for (n = 0; n < check[i].num; n++) {
+        printf("#%d: partiton %2d: protect %d\n", n, p[n].partition, p[n].protect);
+      }
+
+      device_set_symbol_address(DEVICE_SYMBOL(mmc_protect_part), addr);
+      device_set_symbol_address(DEVICE_SYMBOL(mmc_protect.part_type),  check[i].type);
+
+      ret = true;
+      break;
+    }
+  }
+
+error_exit:
+  kallsyms_in_memory_free(info);
+  return ret;
+}
+
+static bool
+setup_param_from_database(void)
+{
+  int i;
+
+  if (mmc_protect_part) {
+    if (mmc_protect_part_type == MMC_PROTECT_PART_TYPE1
+     || mmc_protect_part_type == MMC_PROTECT_PART_TYPE2) {
+      return true;
+    }
+  }
+
+  mmc_protect_part = device_get_symbol_address(DEVICE_SYMBOL(mmc_protect_part));
+  if (!mmc_protect_part) {
+    detect_mmc_protect();
+
+    mmc_protect_part = device_get_symbol_address(DEVICE_SYMBOL(mmc_protect_part));
+  }
+
+  mmc_protect_part_type = device_get_symbol_address(DEVICE_SYMBOL(mmc_protect.part_type));
+
+  if (mmc_protect_part) {
+    if (mmc_protect_part_type == MMC_PROTECT_PART_TYPE1
+     || mmc_protect_part_type == MMC_PROTECT_PART_TYPE2) {
+      return true;
+    }
+  }
+
+  mmc_protect_part = 0;
+  mmc_protect_part_type = MMC_PROTECT_PART_TYPE_UNKNOWN;
+
+  return false;
+}
 
 static bool
 do_unlock(void)
@@ -143,6 +275,14 @@ do_unlock(void)
     return false;
   }
 
+  if (!setup_param_from_database()) {
+    backdoor_close_mmap();
+
+    print_reason_device_not_supported();
+
+    return false;
+  }
+
   ret = unlock_mmc_protect_part();
 
   backdoor_close_mmap();
@@ -152,81 +292,75 @@ do_unlock(void)
 int
 main(int argc, char **argv)
 {
-  check_mmc_protect_part = check_mmc_protect_part_type1;
-  n_mmc_protect_part = n_mmc_protect_part_type1;
-  mmc_system_partition = MMC_SYSTEM_PARTITION_TYPE1;
-
   switch (detect_device()) {
   case DEVICE_SBM203SH_S0024:
     mmc_protect_part = mmc_protect_part_sbm203sh_s0024;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE1;
     break;
 
   case DEVICE_SH04E_01_00_02:
     mmc_protect_part = mmc_protect_part_sh04e_01_00_02;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE1;
     break;
 
   case DEVICE_SH04E_01_00_03:
     mmc_protect_part = mmc_protect_part_sh04e_01_00_03;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE1;
     break;
 
   case DEVICE_SH04E_01_00_04:
     mmc_protect_part = mmc_protect_part_sh04e_01_00_04;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE1;
     break;
 
   case DEVICE_SH05E_01_00_05:
     mmc_protect_part = mmc_protect_part_sh05e_01_00_05;
-    check_mmc_protect_part = check_mmc_protect_part_type2;
-    n_mmc_protect_part = n_mmc_protect_part_type2;
-    mmc_system_partition = MMC_SYSTEM_PARTITION_TYPE2;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE2;
     break;
 
   case DEVICE_SH05E_01_00_06:
     mmc_protect_part = mmc_protect_part_sh05e_01_00_06;
-    check_mmc_protect_part = check_mmc_protect_part_type2;
-    n_mmc_protect_part = n_mmc_protect_part_type2;
-    mmc_system_partition = MMC_SYSTEM_PARTITION_TYPE2;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE2;
     break;
 
   case DEVICE_SH06E_01_00_01:
     mmc_protect_part = mmc_protect_part_sh06e_01_00_01;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE1;
     break;
 
   case DEVICE_SH06E_01_00_06:
     mmc_protect_part = mmc_protect_part_sh06e_01_00_06;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE1;
     break;
 
   case DEVICE_SH06E_01_00_07:
     mmc_protect_part = mmc_protect_part_sh06e_01_00_07;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE1;
     break;
 
   case DEVICE_SH07E_01_00_03:
     mmc_protect_part = mmc_protect_part_sh07e_01_00_03;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE1;
     break;
 
   case DEVICE_SH09D_02_00_03:
-    check_mmc_protect_part = check_mmc_protect_part_type2;
-    n_mmc_protect_part = n_mmc_protect_part_type2;
     mmc_protect_part = mmc_protect_part_sh09d_02_00_03;
-    mmc_system_partition = MMC_SYSTEM_PARTITION_TYPE2;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE2;
     break;
 
   case DEVICE_SHL21_01_00_09:
-    check_mmc_protect_part = check_mmc_protect_part_type2;
-    n_mmc_protect_part = n_mmc_protect_part_type2;
     mmc_protect_part = mmc_protect_part_shl21_01_00_09;
-    mmc_system_partition = MMC_SYSTEM_PARTITION_TYPE2;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE2;
     break;
 
   case DEVICE_SHL21_01_01_02:
-    check_mmc_protect_part = check_mmc_protect_part_type2;
-    n_mmc_protect_part = n_mmc_protect_part_type2;
     mmc_protect_part = mmc_protect_part_shl21_01_01_02;
-    mmc_system_partition = MMC_SYSTEM_PARTITION_TYPE2;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE2;
     break;
 
   default:
-    print_reason_device_not_supported();
-    return 1;
+    mmc_protect_part = 0;
+    mmc_protect_part_type = MMC_PROTECT_PART_TYPE_UNKNOWN;
   }
 
   if (!do_unlock()) {
